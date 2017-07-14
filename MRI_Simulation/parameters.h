6f64c6d1e3b1b78728a486ef41a0cc0e9132fc86
RabinKarp <vbharadw@caltech.edu>
2017-07-14 16:42:37 -0700

    Version of the code pulled from the server on July 14, 2017

--- MRI_Simulation/parameters.h
+++ MRI_Simulation/parameters.h
@@ -30,11 +30,11 @@ const int num_runs = 1;     // number of times to run T2 simulation
 
 /* Molecule and nanoparticle info */
 const int num_water = 2000;              // number of waters in simulation
-const double mnp_radius = (.1)/3;           // radius of one nanoparticle (um) (.1 NV)
-#define EXTRACELLULAR                    // MNPs intracellular or extracellular?
+const double mnp_radius = .1;           // radius of one nanoparticle (um) (.1 NV)
+#undef EXTRACELLULAR                    // MNPs intracellular or extracellular?
 #ifdef EXTRACELLULAR
 const int num_mnps = (1.760e3);           // number of MNPs (if all extracellular) (1.76e3 NV)
-const double mmoment = (1.7e-15)/27;           // magnetic moment of each MNP (1.7e-15 NV)
+const double mmoment = 1.7e-15;           // magnetic moment of each MNP (1.7e-15 NV)
 const double scale = raw_scale;         // to account for smaller MNPs
 #else
 const double mnp_pack = 3;              // influences MNP cluster packing
@@ -42,7 +42,7 @@ const double scale = raw_scale;         // to account for larger MNPs
 #endif
 
 /* Characteristics of FCC cell lattice */
-const double cell_r = 6.8;                // cell radius in microns
+const double cell_r = 9;                // cell radius in microns
 const double prob_labeled = 0.26;       // probability a given cell is labeled
 const double fcc_pack = 1.00;           // influences FCC packing efficiency
 const double bound = 6*sqrt(2)*cell_r*fcc_pack; // full box is [0, bound]^3
@@ -50,7 +50,7 @@ const double bound = 6*sqrt(2)*cell_r*fcc_pack; // full box is [0, bound]^3
 /* Constants affecting diffusion */
 const double D_cell = .5547;            // D in micron^2 per ms
 const double D_extra = 1.6642;          // D in micron^2 per ms
-const double P_expr = 0.034;             // permeability in micron per ms (.034 NV)
+const double P_expr = 0.01;             // permeability in micron per ms
 
 /* Time scales and step sizes */
 const double tau = 1e-6;                // time step in ms (1e-6 NV)
